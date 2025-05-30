2f07776ef1ec5edc47dac73cd4ccdf03374816eb
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-05-30 16:17:55 +0300

    Update collect_node_data.sh

--- collect_node_data.sh
+++ collect_node_data.sh
@@ -3,7 +3,9 @@
 BASE_DIR="/opt/gaianet-run/data"
 OUTPUT_FILE="node_info.csv"
 
-echo "node,device_id,address,keystore,password" > "$OUTPUT_FILE"
+echo "node,server,address,device_id,keystore,password" > "$OUTPUT_FILE"
+
+SERVER_IP=$(curl -4 ifconfig.me)
 
 for NODE_DIR in "$BASE_DIR"/node*/gaianet; do
   NODE_NAME=$(basename "$(dirname "$NODE_DIR")")
@@ -16,7 +18,7 @@ for NODE_DIR in "$BASE_DIR"/node*/gaianet; do
     KEYSTORE=$(jq -r '.keystore' "$NODEID_FILE")
     PASSWORD=$(jq -r '.password' "$NODEID_FILE")
 
-    echo "$NODE_NAME,$DEVICE_ID,$ADDRESS,$KEYSTORE,$PASSWORD" >> "$OUTPUT_FILE"
+    echo "$NODE_NAME,$SERVER_IP,$ADDRESS,$DEVICE_ID,$ADDRESS,$KEYSTORE,$PASSWORD" >> "$OUTPUT_FILE"
   else
     echo "⚠️ Пропущено: $NODE_NAME — отсутствует deviceid.txt или nodeid.json"
   fi
