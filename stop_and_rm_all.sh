7fb7d0794e1d0f939d0dd944500fc614c5fb3a42
JeTr1x <70804056+JeTr1x@users.noreply.github.com>
2025-07-29 17:46:33 +0300

    Create stop_and_rm_all.sh

---
+++ stop_and_rm_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Остановка всех контейнеров gaianet-node-[1-100]..."
+
+for i in $(seq 1 40); do
+  CONTAINER_NAME="gaianet-node-$i"
+  if docker ps -q -f name="^/${CONTAINER_NAME}$" > /dev/null; then
+    echo "Останавливаю $CONTAINER_NAME..."
+    docker stop "$CONTAINER_NAME"
+    docker rm -f "$CONTAINER_NAME"
+  else
+    echo "$CONTAINER_NAME не запущен."
+  fi
+done
